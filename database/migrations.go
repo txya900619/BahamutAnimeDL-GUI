6e0dd5b48272be532f9a0b0d58f12272fa58605f
txya900619 <wayne900619@gmail.com>
2020-05-23 22:30:45 +0800

    delete working dir

--- database/migrations.go
+++ database/migrations.go
@@ -6,14 +6,10 @@ import (
 	"github.com/markbates/pkger"
 	migrate "github.com/rubenv/sql-migrate"
 	"net/http"
-	"os"
 )
 
 func migrations(db *sql.DB) {
-	workingDir, err := os.Getwd()
-	if err != nil {
-		fmt.Println(err)
-	}
+
 	migrationSource := &migrate.HttpFileSystemMigrationSource{
 		FileSystem: http.FileSystem(pkger.Dir("/migrations")),
 	}
