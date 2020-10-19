14b31460fb28fcf7999183fa05a3bd324ccb66d4
txya900619 <wayne900619@gmail.com>
2020-10-20 02:17:02 +0800

    migration from lorca to webview, and delete crawler package, and upgrade third party packages

--- main.go
+++ main.go
@@ -13,10 +13,8 @@ import (
 	"os"
 	"runtime"
 	"strconv"
-	"strings"
 
 	"github.com/markbates/pkger"
-	"github.com/txya900619/BahamutAnimeDL-GUI/crawler"
 	"github.com/txya900619/BahamutAnimeDL-GUI/database"
 	dbModels "github.com/txya900619/BahamutAnimeDL-GUI/database/models"
 	"github.com/txya900619/BahamutAnimeDL-GUI/models"
@@ -24,18 +22,14 @@ import (
 	"github.com/txya900619/BahamutAnimeDL-GUI/utilities"
 	"github.com/volatiletech/sqlboiler/v4/boil"
 	"github.com/volatiletech/sqlboiler/v4/queries/qm"
-	"github.com/zserge/lorca"
+	"github.com/webview/webview"
 )
 
-var NewAnimeList []models.NewAnime
-var AnimeList []models.Anime
 var db *sql.DB
 var queueSystem *queue.System
 
 func init() {
 	db = database.ConnectSqlite()
-	NewAnimeList = crawler.GetNewAnimeList()
-	AnimeList = crawler.GetAllAnimeList()
 
 	if _, err := os.Stat("./.temp"); os.IsNotExist(err) {
 		os.Mkdir("./.temp", 0777)
@@ -49,52 +43,14 @@ func init() {
 }
 
 func main() {
-	args := []string{}
-	if runtime.GOOS == "linux" {
-		args = append(args, "--class=Lorca")
-	}
-	args = append(args, "--disable-features=TranslateUI")
-
-	app, err := lorca.New("", "", 1200, 800, args...)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer app.Close()
-
-	app.Bind("getNewAnimeList", func() []models.NewAnime {
-		return NewAnimeList
-	})
-
-	app.Bind("getAnimesByPage", func(page int) []models.Anime {
-		return AnimeList[(page-1)*18 : page*18]
-	})
-	app.Bind("getMaxPage", func() int {
-		return len(AnimeList)/18 + 1
-	})
-
-	app.Bind("getAnimesByFilter", func(filter string) []models.Anime {
-		filter = strings.ToLower(filter)
-		filteredAnimes := make([]models.Anime, 0)
-		for _, v := range AnimeList {
-			if strings.Contains(strings.ToLower(v.Title), filter) {
-				filteredAnimes = append(filteredAnimes, v)
-			}
-		}
-		return filteredAnimes
-	})
-
-	app.Bind("getRealSn", func(ref string) string {
-		return crawler.GetRealSn(ref)
-	})
-
-	app.Bind("getAnimeAllSn", func(title, sn string) map[string][]models.Sn {
-		return crawler.GetSnsByOneSn(title, sn, db)
-	})
+	app := webview.New(true)
+	defer app.Destroy()
+	app.SetSize(1200, 800, webview.HintNone)
+	app.SetTitle("")
 
 	app.Bind("insertAnimeToQueue", func(title, ep, sn string, spacial bool) bool {
-		if crawler.Check18Up(sn) {
-			return false
-		}
+		//TODO: maybe test 18
+
 		lastSequence, err := dbModels.DownloadQueues().Count(context.Background(), db)
 		if err != nil {
 			log.Fatal(err)
@@ -107,8 +63,8 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		queue := dbModels.DownloadQueue{SN: intSn, Name: title, Ep: ep, Sequence: lastSequence + 1, Spacial: intSpacial}
-		err = queue.Insert(context.Background(), db, boil.Infer())
+		downloadQueue := dbModels.DownloadQueue{SN: intSn, Name: title, Ep: ep, Sequence: lastSequence + 1, Spacial: intSpacial}
+		err = downloadQueue.Insert(context.Background(), db, boil.Infer())
 		return true
 	})
 
@@ -126,16 +82,16 @@ func main() {
 			}
 			queues = append(queues, *queuePtr)
 		}
-		return models.QueueStatus{queues, downloadStatus}
+		return models.QueueStatus{Queue: queues, DownloadingStatus: downloadStatus}
 	})
 
-	net, err := net.Listen("tcp", "127.0.0.1:0")
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer net.Close()
+	defer listener.Close()
 
-	go http.Serve(net, http.FileServer(pkger.Dir("/dist")))
-	app.Load(fmt.Sprintf("http://%s", net.Addr()))
-	<-app.Done()
+	go http.Serve(listener, http.FileServer(pkger.Dir("/dist")))
+	app.Navigate(fmt.Sprintf("http://%s", listener.Addr()))
+
 }
