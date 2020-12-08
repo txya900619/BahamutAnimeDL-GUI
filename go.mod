cf4182236925265c4478eedcf9a7277024e76952
txya900619 <wayne900619@gmail.com>
2020-12-08 17:21:47 +0800

    add /api proxy

--- go.mod
+++ go.mod
@@ -9,9 +9,9 @@ require (
 	github.com/markbates/pkger v0.17.1
 	github.com/mattn/go-sqlite3 v2.0.3+incompatible
 	github.com/rubenv/sql-migrate v0.0.0-20200616145509-8d140a17f351
-	github.com/volatiletech/sqlboiler/v4 v4.2.0
+	github.com/volatiletech/sqlboiler/v4 v4.3.1
 	github.com/volatiletech/strmangle v0.0.1
 	github.com/webview/webview v0.0.0-20200724072439-e0c01595b361
-	golang.org/x/sys v0.0.0-20201018230417-eeed37f84f13
+	golang.org/x/sys v0.0.0-20201202213521-69691e467435
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- main.go
+++ main.go
@@ -3,95 +3,112 @@
 package main
 
 import (
-	"context"
 	"database/sql"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
-	"os"
-	"runtime"
-	"strconv"
+	"strings"
 
 	"github.com/markbates/pkger"
-	"github.com/txya900619/BahamutAnimeDL-GUI/database"
-	dbModels "github.com/txya900619/BahamutAnimeDL-GUI/database/models"
-	"github.com/txya900619/BahamutAnimeDL-GUI/models"
 	"github.com/txya900619/BahamutAnimeDL-GUI/queue"
-	"github.com/txya900619/BahamutAnimeDL-GUI/utilities"
-	"github.com/volatiletech/sqlboiler/v4/boil"
-	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 	"github.com/webview/webview"
 )
 
 var db *sql.DB
 var queueSystem *queue.System
 
-func init() {
-	db = database.ConnectSqlite()
+// func init() {
+// 	db = database.ConnectSqlite()
 
-	if _, err := os.Stat("./.temp"); os.IsNotExist(err) {
-		os.Mkdir("./.temp", 0777)
-		if runtime.GOOS == "windows" {
-			utilities.HideFolder("./.temp")
-		}
-	}
+// 	if _, err := os.Stat("./.temp"); os.IsNotExist(err) {
+// 		os.Mkdir("./.temp", 0777)
+// 		if runtime.GOOS == "windows" {
+// 			utilities.HideFolder("./.temp")
+// 		}
+// 	}
+// 	queueSystem = queue.New(db, 1)
+// 	go queueSystem.Start()
 
-	queueSystem = queue.New(db, 1)
-	go queueSystem.Start()
-}
+// }
 
 func main() {
 	app := webview.New(true)
 	defer app.Destroy()
 	app.SetSize(1200, 800, webview.HintNone)
 	app.SetTitle("")
+	// app.Bind("insertAnimeToQueue", func(title, ep, sn string, spacial bool) bool {
+	// 	//TODO: maybe test 18
 
-	app.Bind("insertAnimeToQueue", func(title, ep, sn string, spacial bool) bool {
-		//TODO: maybe test 18
+	// 	lastSequence, err := dbModels.DownloadQueues().Count(context.Background(), db)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	var intSpacial int64
+	// 	if spacial {
+	// 		intSpacial = 1
+	// 	}
+	// 	intSn, err := strconv.ParseInt(sn, 10, 64)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	downloadQueue := dbModels.DownloadQueue{SN: intSn, Name: title, Ep: ep, Sequence: lastSequence + 1, Spacial: intSpacial}
+	// 	err = downloadQueue.Insert(context.Background(), db, boil.Infer())
+	// 	return true
+	// })
 
-		lastSequence, err := dbModels.DownloadQueues().Count(context.Background(), db)
-		if err != nil {
-			log.Fatal(err)
-		}
-		var intSpacial int64
-		if spacial {
-			intSpacial = 1
-		}
-		intSn, err := strconv.ParseInt(sn, 10, 64)
-		if err != nil {
-			log.Fatal(err)
-		}
-		downloadQueue := dbModels.DownloadQueue{SN: intSn, Name: title, Ep: ep, Sequence: lastSequence + 1, Spacial: intSpacial}
-		err = downloadQueue.Insert(context.Background(), db, boil.Infer())
-		return true
-	})
+	// app.Bind("getDownloadQueue", func() models.QueueStatus {
+	// 	queuesPtr, err := dbModels.DownloadQueues(qm.OrderBy("sequence")).All(context.Background(), db)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	queues := make([]dbModels.DownloadQueue, 0)
+	// 	downloadStatus := make([]int, 0)
+	// 	for _, queuePtr := range queuesPtr {
+	// 		if queuePtr.Downloading == 1 {
+	// 			files, _ := ioutil.ReadDir("./.temp/" + strconv.FormatInt(queuePtr.SN, 10))
+	// 			downloadStatus = append(downloadStatus, len(files))
+	// 		}
+	// 		queues = append(queues, *queuePtr)
+	// 	}
+	// 	return models.QueueStatus{Queue: queues, DownloadingStatus: downloadStatus}
+	// })
 
-	app.Bind("getDownloadQueue", func() models.QueueStatus {
-		queuesPtr, err := dbModels.DownloadQueues(qm.OrderBy("sequence")).All(context.Background(), db)
-		if err != nil {
-			log.Fatal(err)
-		}
-		queues := make([]dbModels.DownloadQueue, 0)
-		downloadStatus := make([]int, 0)
-		for _, queuePtr := range queuesPtr {
-			if queuePtr.Downloading == 1 {
-				files, _ := ioutil.ReadDir("./.temp/" + strconv.FormatInt(queuePtr.SN, 10))
-				downloadStatus = append(downloadStatus, len(files))
-			}
-			queues = append(queues, *queuePtr)
-		}
-		return models.QueueStatus{Queue: queues, DownloadingStatus: downloadStatus}
-	})
-
-	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	listener, err := net.Listen("tcp", "127.0.0.1:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer listener.Close()
 
-	go http.Serve(listener, http.FileServer(pkger.Dir("/dist")))
+	serverMux := http.NewServeMux()
+
+	serverMux.Handle("/", http.FileServer(pkger.Dir("/dist")))
+	serverMux.HandleFunc("/api/", func(w http.ResponseWriter, r *http.Request) {
+		url := strings.Replace(r.URL.Path, "/api", "https://api.gamer.com.tw/mobile_app", -1)
+		var res *http.Response
+		if r.Method == http.MethodGet {
+			res, err = http.Get(url)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+		}
+		//TODO: POST method
+
+		resBody, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		w.WriteHeader(res.StatusCode)
+		w.Write(resBody)
+	})
+
+	go http.Serve(listener, serverMux)
+
 	app.Navigate(fmt.Sprintf("http://%s", listener.Addr()))
+	app.Run()
 
 }
